cce620f7c6a1c002ed8f3e527a69fe62930e98b3
Andrew Zhang <snowtown86@gmail.com>
2012-03-28 09:52:18 +0800

    JDBC: a perfect jdbc connection example.

--- JDBC_Test/src/Hello_JDBC.java
+++ JDBC_Test/src/Hello_JDBC.java
@@ -1,12 +1,66 @@
 
+import java.sql.*;
+
 public class Hello_JDBC {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://localhost:3306/jdbc?user=root&password=amigo";
+			conn = DriverManager.getConnection(url);
+			
+			stmt = conn.createStatement();
+			String sql = "select * from userlist";
+			rs = stmt.executeQuery(sql);
+			
+			while (rs.next()) {
+				System.out.println(rs.getInt("id"));
+				System.out.println(rs.getString("name"));
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+					rs = null;
+				}
+				if (stmt != null) {
+					stmt.close();
+					stmt = null;
+				}
+				if (conn != null) {
+					conn.close();
+					conn = null;
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
