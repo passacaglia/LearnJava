16e22c870f2ed6ace64ff4775000fd38ed130607
Andrew Zhang <snowtown86@gmail.com>
2012-03-28 14:14:27 +0800

    JDBC:PreparedStatement example.

---
+++ JDBC_Test/src/PreparedStmt.java
@@ -0,0 +1,71 @@
+import java.sql.*;
+public class PreparedStmt {
+
+	public static void main(String[] args) {
+		String name = null;
+		
+		if (1 != args.length) {
+			System.out.println("Usage: \n java PreparedStmt userName \n System exit!");
+			System.exit(-1);
+		}
+		
+		name = args[0];
+		
+		Connection conn = null;
+		PreparedStatement prstmt = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://localhost:3306/jdbc";
+			conn = DriverManager.getConnection(url, "root", "amigo");
+			
+			String sql = "insert into userlist values (null, ?);";
+			prstmt = conn.prepareStatement(sql);
+			prstmt.setString(1, name);//1 : 第一个问号的值。//貌似String也不需要''了。
+			prstmt.executeUpdate();
+System.out.println(sql);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (prstmt != null) {
+					prstmt.close();
+					prstmt = null;
+				}
+				if (conn != null) {
+					conn.close();
+					conn = null;
+				}
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
