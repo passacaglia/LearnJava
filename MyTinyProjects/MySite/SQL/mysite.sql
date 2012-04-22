b17fd61f0524eb481a4084ccb4a948d5dd990e98
Andrew Zhang <snowtown86@gmail.com>
2012-04-22 21:52:49 +0800

    MySite: edit sql file and create database

--- MyTinyProjects/MySite/SQL/mysite.sql
+++ MyTinyProjects/MySite/SQL/mysite.sql
@@ -2,34 +2,24 @@ create database mysite;
 
 use mysite; 
 
-create table news
-( 
-id int primary key auto_increment, 
-topic varchar(255),
+create table news (
+id int primary key auto_increment,
+title varchar(255),
 content text,
-publishtime datatime,
-publishuser varchar(50),
-hits int,
-categoryid int, 
-picture varchar(100) ); 
+publishtime datetime );
 
-create table news_category
-(
-id int primary key auto_increment,
-name varchar(20),
-master varchar(20)
-);
+insert into news values (null, 'news 001', 'news 001 content', now() );
+insert into news values (null, '蚂蚁大战大象', '蚂蚁大战大象', now() );
+insert into news values (null, '大象被打趴下了', '大象被打趴下了',now() );
+insert into news values (null, '蚂蚁也不好过','蚂蚁也不好过', now() );
+insert into news values (null, '瞎说', '瞎说', now() );
+insert into news values (null, '没有瞎说', '没有瞎说', now());
+insert into news values (null, '怎么可能', '怎么可能', now());
+insert into news values (null, '怎么没有可能', '怎么没有可能', now());
+insert into news values (null, '可能性是很大的', '可能性是很大的', now());
+insert into news values (null, '大象进医院了', '大象进医院了', now());
+insert into news values (null, '护士是蚂蚁', '护士是蚂蚁', now());
 
-create table news_master
-(
-id int primary key auto_increment,
-username varchar(20),
-userpwd varchar(20),
-total int,
-categoryid int,
-loginnum int,
-registertime datetime
-);
 
 
 
