8e5e7ec13e02c1242ed43b090bc18f38296574e4
Andrew Zhang <snowtown86@gmail.com>
2012-04-23 09:06:05 +0800

    MySite: insert into table new data.

--- MyTinyProjects/MySite/SQL/mysite.sql
+++ MyTinyProjects/MySite/SQL/mysite.sql
@@ -8,23 +8,18 @@ title varchar(255),
 content text,
 publishtime datetime );
 
-insert into news values (null, 'news 001', 'news 001 content', now() );
-insert into news values (null, '蚂蚁大战大象', '蚂蚁大战大象', now() );
-insert into news values (null, '大象被打趴下了', '大象被打趴下了',now() );
-insert into news values (null, '蚂蚁也不好过','蚂蚁也不好过', now() );
-insert into news values (null, '瞎说', '瞎说', now() );
-insert into news values (null, '没有瞎说', '没有瞎说', now());
-insert into news values (null, '怎么可能', '怎么可能', now());
-insert into news values (null, '怎么没有可能', '怎么没有可能', now());
-insert into news values (null, '可能性是很大的', '可能性是很大的', now());
-insert into news values (null, '大象进医院了', '大象进医院了', now());
-insert into news values (null, '护士是蚂蚁', '护士是蚂蚁', now());
-
-
-
-
-
-
+insert into news values (null, '1.症状：消沉绝望；', '电影处方：《小鞋子》《肖申克的救赎》。', now() );
+insert into news values (null, ' 2.症状：心理抑郁；', '电影处方：《谈谈情跳跳舞》。', now() );
+insert into news values (null, '3.症状：自卑失落；', '电影处方：《阿甘正传》。', now() );
+insert into news values (null, '4.症状：交流障碍；', '电影处方：《对她说》。', now() );
+insert into news values (null, '5.症状：自私冷漠；', '电影处方：《爱·回家》', now() );
+insert into news values (null, '20部练口语经典电影(1)', '1.阿甘正传,2.电子情书 3.居家男人,4.公主日记,5.逃跑的新娘, ', now() );
+insert into news values (null, '20部练口语经典电影(2)', '6.新岳父大人,7.BJ单身日记,8.奔腾年代,9.蒙娜丽莎的微笑,10.甜心先生,', now() );
+insert into news values (null, '20部练口语经典电影(3)', '11.热血强人,12.铁血教练,13.重建人生,14.海上钢琴师,15.大话王,', now() );
+insert into news values (null, '20部练口语经典电影(4)', '16.单身插班生,17.风流奇男子,18.妙手情真,19.完美男人,20.大鱼。', now() );
+insert into news values (null, '让你心情好起来的12部电影(1)', '《神探飞机头》《我盛大的同志婚礼》《幸运库克》《校园卧底》', now() );
+insert into news values (null, '让你心情好起来的12部电影(2)', '《我的野蛮老师》《美女也烦恼》《疯狂的石头》《谋杀快乐》', now() );
+insert into news values (null, '让你心情好起来的12部电影(3)', '《少儿不宜》《我为玛丽狂》《美国派》《偷天情缘》', now() );
 
 
 
