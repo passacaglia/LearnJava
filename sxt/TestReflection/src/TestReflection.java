9e8ddfda860fa39a1c0c2eb66d6490fc974aab1f
Andrew Zhang <snowtown86@gmail.com>
2012-05-03 08:55:12 +0800

    TestReflection:test load order.

---
+++ sxt/TestReflection/src/TestReflection.java
@@ -0,0 +1,44 @@
+
+public class TestReflection {
+
+	/**
+	 * When you run this program, please add the 
+	 * VM arguments: -verbose:class .
+	 */
+	public static void main(String[] args) {
+		//按需要加载
+		new A();
+		System.out.println("******Where am I ?******");
+		new B();
+		
+		//static,一次。dynamic,每次。
+		new C();
+		new C();
+		new D();
+		new D();
+
+	}
+
+}
+
+class A {
+	
+}
+
+class B {
+	
+}
+
+class C {
+	//static, load one time.
+	static {
+		System.out.println("*******Hello, I'm C :) !*******");
+	}
+}
+
+class D {
+	//dynamic , load once the object is created.(用 的少)
+	{
+		System.out.println("*******Hello, I'm D :D !*******");
+	}
+}
