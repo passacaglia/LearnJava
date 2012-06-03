e72bff47e55c2424ba1a84718c121bf259e621e8
Andrew Zhang <snowtown86@gmail.com>
2012-06-03 17:04:29 +0800

    JDBC:sqlite test ok while oracle bad.

--- JDBC_Test/src/com/az/oracle/HelloOracle.java
+++ JDBC_Test/src/com/az/oracle/HelloOracle.java
@@ -1,13 +1,16 @@
 package com.az.oracle;
 
+import java.sql.*;
+
 public class HelloOracle {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		String url = "jdbc:oracle:@127.0.0.1:1521";
+		Connection conn = DriverManager.getConnection(url, "scott", "tiger");
 	}
 
 }
