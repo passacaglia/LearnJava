9e35583aa46a96aa97ba0d8e6253a5a491430ece
Andrew Zhang <snowtown86@gmail.com>
2012-06-03 11:42:23 +0800

    FT:SetClipboard content to something you want.

---
+++ MyTinyProjects/FunctionalTools/src/com/az/SetClipboard.java
@@ -0,0 +1,19 @@
+package com.az;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+
+public class SetClipboard {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		Clipboard cb = tk.getSystemClipboard();
+		StringSelection ss = new StringSelection("qwerT");//The string that you want to set.
+		cb.setContents(ss, ss);
+	}
+
+}
