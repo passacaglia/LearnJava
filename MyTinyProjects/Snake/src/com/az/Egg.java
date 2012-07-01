3d19dc1d94974b9b03d7888841e75a2c61073196
Andrew Zhang <snowtown86@gmail.com>
2012-07-01 21:13:33 +0800

    Snake:initial commit.

---
+++ MyTinyProjects/Snake/src/com/az/Egg.java
@@ -0,0 +1,5 @@
+package com.az;
+
+public class Egg {
+
+}
---
+++ MyTinyProjects/Snake/src/com/az/Yard.java
@@ -0,0 +1,14 @@
+package com.az;
+import java.awt.Frame;
+
+
+public class Yard extends Frame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
