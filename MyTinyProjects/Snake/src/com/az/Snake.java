e0457081d9e34060009d3e7bc1ef59e5f09843c3
Andrew Zhang <snowtown86@gmail.com>
2012-07-02 22:29:09 +0800

    Snake: addToTail() & addToHead().

--- MyTinyProjects/Snake/src/com/az/Snake.java
+++ MyTinyProjects/Snake/src/com/az/Snake.java
@@ -18,6 +18,54 @@ public class Snake {
 	}
 	
 	
+	public void addToTail() {
+		Node node = null;
+		switch(tail.dir) {
+		case L :
+			node = new Node(tail.row, tail.col+1, tail.dir);
+			break;
+		case U :
+			node = new Node(tail.row+1, tail.col, tail.dir);
+			break;
+		case R :
+			node = new Node(tail.row, tail.col-1, tail.dir);
+			break;
+		case D :
+			node = new Node(tail.row-1, tail.col, tail.dir);
+			break;
+		}
+		
+		tail.next = node;
+		node.prev = tail;
+		tail = node;
+		size ++;
+	}
+	
+	
+	public void addToHead() {
+		Node node = null;
+		switch(head.dir) {
+		case L :
+			node = new Node(head.row, head.col-1, head.dir);
+			break;
+		case U :
+			node = new Node(tail.row-1, tail.col, head.dir);
+			break;
+		case R :
+			node = new Node(head.row, head.col+1, head.dir);
+			break;
+		case D :
+			node = new Node(head.row+1, head.col, head.dir);
+			break;
+		}
+		
+		node.next = head;
+		head.prev = node;
+		head = node;
+		size ++;
+	}
+	
+	
 	public void draw(Graphics g) {
 		if (size <= 0) {
 			return;
